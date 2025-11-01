430fb5e098e14dcaee5fdc13d076c975995c825b
lizhuoran <liZhuoran1998@outlook.com>
2025-11-02 05:09:26 +0800

    Add Event

---
+++ include/Event.hpp
@@ -0,0 +1,142 @@
+#include <cstdint>
+#include <atomic>
+#include <tuple>
+#include <functional>
+#include <utility>
+
+#include "Utilities.hpp"
+
+struct EventHandle: NonCopyMove<EventHandle> 
+{
+public:
+	enum class GenerateType : uint8_t 
+	{
+		Empty = 0,
+		Valid = 1
+	};
+	explicit EventHandle()
+	 : id(0)
+	{
+		
+	}
+	explicit EventHandle(GenerateType type)
+	 : id(type == GenerateType::Valid ? GenerateNextId() : 0)
+	{
+		
+	}
+
+	bool operator==(const EventHandle& other) const noexcept 
+	{
+		return id == other.id;
+	}
+
+	bool operator!=(const EventHandle& other) const noexcept 
+	{
+		return id != other.id;
+	}
+
+	~EventHandle() = default;
+public:
+	bool IsValid() const noexcept 
+	{
+		return id != 0;
+	}	
+
+	void Reset() noexcept 
+	{
+		id = 0;
+	}
+private:
+	static uint64_t GenerateNextId() noexcept
+	{
+		static std::atomic<uint64_t> id {0};
+		uint64_t retNextId = ++id;
+
+		/* 防止溢出 */
+		if (retNextId == 0) {
+			retNextId = ++id;
+		}
+		return retNextId;
+	}
+	uint64_t id;
+};
+
+template <typename RetVal, typename... Args>
+struct EventAction
+{
+	using Params = std::tuple<Args...>;
+	using RetTp  = RetVal;
+	using Action = std::function<RetTp(Args...)>;
+	
+	explicit EventAction(Args ... args)
+	 : params(std::make_tuple(std::forward<Args>(args)...))
+	{
+		
+	}
+
+	uint8_t GetParamsNum() const noexcept 
+	{
+		return sizeof...(Args);
+	}
+
+	template <uint8_t Index>
+	constexpr auto GetParamAt() const noexcept
+		-> const std::tuple_element_t<Index, Params>&
+	{
+		static_assert(Index < sizeof...(Args), "Index out of bounds");
+		return std::get<Index>(params);
+	}
+
+	template <uint8_t Index>
+	constexpr auto GetParamAt() noexcept
+		-> std::tuple_element_t<Index, Params>&
+	{
+		static_assert(Index < sizeof...(Args), "Index out of bounds");
+		return std::get<Index>(params);
+	}
+	
+	void Bind(Action action) noexcept
+	{
+		this->action = action;
+	}
+
+    RetTp Execute() {
+        if (!action) {
+            throw std::bad_function_call();
+        }
+
+        if constexpr (std::is_void_v<RetTp>) {
+            // 非 const 版本允许移动 params（避免不必要拷贝）
+            std::apply(action, std::move(params));
+        } else {
+            return std::apply(action, std::move(params));
+        }
+    }
+
+    RetTp Execute() const {
+        if (!action) {
+            throw std::bad_function_call();
+        }
+
+        if constexpr (std::is_void_v<RetTp>) {
+            // const 版本使用 const 引用，不会移动 members
+            std::apply(action, params);
+        } else {
+            return std::apply(action, params);
+        }
+    }
+
+private:
+	Params params;
+	Action action;
+};
+
+class Event
+{
+public:
+	
+
+private:
+
+
+};
---
+++ include/Utilities.hpp
@@ -0,0 +1,53 @@
+#pragma once
+#include <utility>
+#include <tuple>
+#include <functional>
+#include <type_traits>
+
+// 删除拷贝操作（派生类会自动变为不可拷贝）
+template<typename Tag = void>
+struct NonCopy {
+protected:
+    NonCopy() = default;
+    ~NonCopy() = default;
+
+    NonCopy(const NonCopy&) = delete;
+    NonCopy& operator=(const NonCopy&) = delete;
+};
+
+// 删除移动操作（派生类会自动变为不可移动）
+template<typename Tag = void>
+struct NonMove {
+protected:
+    NonMove() = default;
+    ~NonMove() = default;
+
+    NonMove(NonMove&&) = delete;
+    NonMove& operator=(NonMove&&) = delete;
+};
+
+// 同时删除拷贝与移动（可通过 Tag 区分相同基类的多重继承）
+template<typename Tag = void>
+struct NonCopyMove : NonCopy<Tag>, NonMove<Tag> {
+protected:
+    NonCopyMove() = default;
+    ~NonCopyMove() = default;
+};
+
+
+/*
+ * @brief 使用元组解包调用函数, 用C++17之前没有std::apply的环境
+ */
+template <typename Func, typename Tuple, std::size_t... I>
+decltype(auto) ApplyWithUnpackedTupleImpl(Func&& func, Tuple&& tuple, std::index_sequence<I...>) {
+    return std::forward<Func>(func)(std::get<I>(std::forward<Tuple>(tuple))...);
+}
+
+template <typename Func, typename Tuple>
+decltype(auto) ApplyWithUnpackedTuple(Func&& func, Tuple&& tuple) {
+    return ApplyWithUnpackedTupleImpl(
+        std::forward<Func>(func),
+        std::forward<Tuple>(tuple),
+        std::make_index_sequence<std::tuple_size<std::decay_t<Tuple>>::value>{}
+    );
+}
